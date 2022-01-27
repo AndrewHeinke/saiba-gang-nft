3b0e8e146ef3f8e6332b11fc2fc7b653ab00f6b6
AndrewHeinke <andrew.j.heinke@gmail.com>
2022-01-26 20:40:20 -0600

    refactor: update notification banner styles

--- src/components/NotificationBanner.js
+++ src/components/NotificationBanner.js
@@ -3,23 +3,22 @@ import Container from "./Container";
 
 export default function NotifcationBanner() {
   return (
-    <div className={styles["notification-banner"]}>
+    <a
+      className={styles["notification-banner"]}
+      rel="noreferrer"
+      target="_blank"
+      href="https://www.webtoons.com/en/challenge/saiba-gang/list?title_no=722467"
+    >
       <Container>
         <div className={styles["notification-banner-wrapper"]}>
           <p className={styles["notification-banner-message"]}>
-            We launched a manga! Check out our Webtoons dashboard to stay
-            updated on the latest episodes!
+            We launched a manga!{" "}
+            <span className={styles["notification-banner-underline"]}>
+              Saiba Gang Webtoons
+            </span>
           </p>
-          <a
-            className={styles["notification-banner-link"]}
-            rel="noreferrer"
-            target="_blank"
-            href="https://www.webtoons.com/en/challenge/saiba-gang/list?title_no=722467"
-          >
-            View Webtoons
-          </a>
         </div>
       </Container>
-    </div>
+    </a>
   );
 }
